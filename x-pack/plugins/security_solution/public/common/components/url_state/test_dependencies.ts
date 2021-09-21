84d2671cefb895f2dadc6d1ad5550837b2076080
Pablo Neves Machado <pablo.nevesmachado@elastic.co>
2021-09-21 14:01:09 +0200

    Refactor useUrlStateHook

--- x-pack/plugins/security_solution/public/common/components/url_state/test_dependencies.ts
+++ x-pack/plugins/security_solution/public/common/components/url_state/test_dependencies.ts
@@ -6,13 +6,14 @@
  */
 
 import { ActionCreator } from 'typescript-fsa';
+import { renderHook } from '@testing-library/react-hooks';
 import { DispatchUpdateTimeline } from '../../../timelines/components/open_timeline/types';
 import { navTabs } from '../../../app/home/home_navigations';
 import { SecurityPageName } from '../../../app/types';
 import { inputsActions } from '../../store/actions';
 
 import { CONSTANTS } from './constants';
-import { dispatchSetInitialStateFromUrl } from './initialize_redux_by_url';
+import { useSetInitialStateFromUrl } from './initialize_redux_by_url';
 import { UrlStateContainerPropTypes, LocationTypes } from './types';
 import { Query } from '../../../../../../../src/plugins/data/public';
 import { networkModel } from '../../../network/store';
@@ -127,12 +128,6 @@ export const defaultProps: UrlStateContainerPropTypes = {
     },
     [CONSTANTS.sourcerer]: {},
   },
-  setInitialStateFromUrl: dispatchSetInitialStateFromUrl(mockDispatch),
-  updateTimeline: jest.fn() as unknown as DispatchUpdateTimeline,
-  updateTimelineIsLoading: jest.fn() as unknown as ActionCreator<{
-    id: string;
-    isLoading: boolean;
-  }>,
   history: {
     ...mockHistory,
     location: defaultLocation,
