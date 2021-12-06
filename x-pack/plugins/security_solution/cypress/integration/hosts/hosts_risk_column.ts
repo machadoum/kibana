092bd4ea4aa4020f3d02777e22c64afc2b304059
Pablo Neves Machado <pablo.nevesmachado@elastic.co>
2021-12-06 14:14:47 +0100

    Add cypress test to risk column on all hosts table

--- x-pack/plugins/security_solution/cypress/integration/hosts/hosts_risk_column.ts
+++ x-pack/plugins/security_solution/cypress/integration/hosts/hosts_risk_column.ts
@@ -9,24 +9,25 @@ import { loginAndWaitForPage } from '../../tasks/login';
 
 import { HOSTS_URL } from '../../urls/navigation';
 import { cleanKibana } from '../../tasks/common';
+import { esArchiverLoad, esArchiverUnload } from '../../tasks/es_archiver';
+import { TABLE_CELL } from '../../screens/alerts_details';
+import { kqlSearch } from '../../tasks/security_header';
 
-describe('RiskyHosts KPI', () => {
+describe('All hosts table', () => {
   before(() => {
     cleanKibana();
     esArchiverLoad('risky_hosts');
-    createSpace(testSpaceName);
   });
 
   after(() => {
     esArchiverUnload('risky_hosts');
-    removeSpace(testSpaceName);
   });
 
-  it('it renders', () => {
+  it('it renders risk column', () => {
     loginAndWaitForPage(HOSTS_URL);
+    kqlSearch('host.name: "siem-kibana" {enter}');
 
-    cy.get('[data-test-subj="riskyHostsTotal"]').should('have.text', '0 Risky Hosts');
-    cy.get('[data-test-subj="riskyHostsCriticalQuantity"]').should('have.text', '0 hosts');
-    cy.get('[data-test-subj="riskyHostsHighQuantity"]').should('have.text', '0 hosts');
+    cy.get('[data-test-subj="tableHeaderCell_node.risk_4"]').should('exist');
+    cy.get(`${TABLE_CELL} .euiTableCellContent`).eq(4).should('have.text', 'Low');
   });
 });
